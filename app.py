2a51bd0caa533a34e1e05c2645e7dcd353da4c16
aanandbajaj <aanand222@gmail.com>
2023-09-01 14:00:26 -0400

    Updated requirements.txt and made changes

--- app.py
+++ app.py
@@ -115,4 +115,7 @@ def signup():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    # Use the PORT environment variable if available
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
